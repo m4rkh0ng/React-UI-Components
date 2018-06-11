3e9b66ec3caacb5d6d013e5c2f75851e57ad0bc6
Mark Hong <m4rkh0ng@gmail.com>
2018-06-11 19:34:02 -0400

    still working on the bottom of the card

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div class="card-content-div">
+            <h1 class="card-content-h1">Get started with React</h1>
+            <p class="card-content-p"> React  makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+            <h6 class="card-content-h6">reactjs.org</h6>
+        </div>
+    );
+};
+
+export default CardContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div class="header-title">
+            <h1 class="header-h1">Lambda School</h1>
+            <h2 class="header-h2">@LambdaSchool • 26 jan</h2>
+        </div>
+    );
+};
+
+export default HeaderTitle;
